b5678e9341e51d562c4f24a105241f7a058a8d1c
anveshshettyy <thomigang03@gmail.com>
2024-11-27 18:46:07 +0530

    New Changes

---
+++ Arrays/LeftRotation.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main() {
+
+    int arr[10];
+    int i, j,temp = 0, k, n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+
+    printf("Enter the Elemenets of the array: ");
+    for( i=0; i<n; i++) {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d",&k);
+
+    for ( j= 0; j<k ; j++) {
+        temp = arr[0];
+        for (i=1; i<n; i++) 
+            arr[i-1] = arr[i];
+        
+            arr[n-1] = temp;
+    
+    }
+
+    printf("Rotated Element is:");
+    for( i=0; i<n ; i++) {
+        printf("%d",arr[i]); 
+    }
+
+
+    return 0;
+}
---
+++ Arrays/RightRotation.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[10];
+    int i, j,temp = 0 , k, n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+
+    printf("Enter the Elemenets of the array: ");
+    for( i=0; i<n; i++) {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d",&k);
+
+    for ( j= 0; j<k; j++ ){
+        temp = arr[n-1];
+        for(i = n -1 ; i > 0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+    }
+
+    printf("Rotated Element is:");
+    for( i=0; i<n ; i++) {
+        printf("%d",arr[i]); 
+    }
+
+    return 0;
+}
