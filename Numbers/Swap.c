78a5247517ca79e2563364f68f14f184d2270782
anveshshettyy <thomigang03@gmail.com>
2024-11-27 20:03:57 +0530

    Swapping

--- Numbers/Swap.c
+++ Numbers/Swap.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 
 int main(){
-    int  a, b;
+    int num, originalNum, firstNum, midNum, lastNum,result = 0, mul = 1;
 
-    printf("Enter a and b:");
-    scanf("%d%d", &a , &b);
+    printf("Enter a number to swap the first and last digit:");
+    scanf("%d",&num);
+    originalNum = num;
 
+    lastNum = num % 10;
+    
+    while ( num >= 10 ) {
+        num = num / 10;
+        mul = mul * 10;
+    }
+    firstNum = num;
 
-    int temp = a;
-    a = b;
-    b = temp;
+    midNum = originalNum % mul;
+    midNum = midNum / 10;
+    
 
-    printf("Enter a: %d", a);+
-    printf("Enter b: %d",b);
+    result = ( (lastNum * mul) + ( midNum * 10 )) + firstNum ;
+    printf("The swapped number is: %d",result );
 
     return 0;
 }
