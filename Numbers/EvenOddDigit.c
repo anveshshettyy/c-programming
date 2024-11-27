77251448a551ec2b772dbafd03b868729e516d40
anveshshettyy <thomigang03@gmail.com>
2024-11-27 20:59:45 +0530

    Changes

---
+++ Numbers/EvenOddDigit.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+
+    int num, even=0, odd, rem, EvenMul = 1, OddMul = 1;
+
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    while ( num > 0 ) {
+        rem = num % 10;
+        if ( rem % 2 == 0 ) {
+            even = even + ( rem * EvenMul );
+            EvenMul *= 10;
+            
+
+        } else {
+            odd = odd + ( rem * OddMul );
+            OddMul *= 10;
+        }
+        num = num / 10;
+    }
+
+    printf("Odd numbers are: %d\n",odd);
+    printf("Even numbers are: %d\n",even);
+    
+
+    return 0;
+
+}
