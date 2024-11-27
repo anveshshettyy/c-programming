3f6ab70a6a4381efe4937144b1c7fdacb8a7ce01
anveshshettyy <thomigang03@gmail.com>
2024-11-27 20:51:07 +0530

    Final

--- Numbers/EvenOddDigit.c
+++ Numbers/EvenOddDigit.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int num, even=0, odd, rem, EvenMul = 1, OddMul = 1;
+    int num, even=0, odd, rem, EvenMul = 1, OddMul = 1, EvenSum=0 , OddSum=0, Diff, EvenC = 0, OddC, remm;
 
     printf("Enter a number:");
     scanf("%d",&num);
@@ -12,14 +12,40 @@ int main() {
         if ( rem % 2 == 0 ) {
             even = even + ( rem * EvenMul );
             EvenMul *= 10;
+            
+
         } else {
             odd = odd + ( rem * OddMul );
             OddMul *= 10;
         }
         num = num / 10;
     }
+
+    EvenC = even;
+    OddC = odd;
+
+    while ( EvenC > 0 ) {
+        remm = EvenC % 10;
+        EvenSum += remm; 
+        EvenC = EvenC / 10;
+    }
+
+    remm = 0;
+
+    while ( OddC > 0 ) {
+        remm = OddC % 10;
+        OddSum += remm; 
+        OddC = OddC / 10;
+    }
+
+    Diff = OddSum - EvenSum;
+
     printf("Even numbers are: %d\n",even);
-    printf("Odd numbers are: %d",odd);
+    printf("Odd numbers are: %d\n",odd);
+    printf("Even Sum is: %d\n",EvenSum);
+    printf("Odd Sum is: %d\n",OddSum);
+    printf("Difference is: %d",Diff);
+
 
     return 0;
 }
