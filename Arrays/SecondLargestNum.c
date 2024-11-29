5137c86c029c31e12e3fed3c86a16aeb595552e2
anveshshettyy <thomigang03@gmail.com>
2024-11-29 21:10:53 +0530

    Second Larges Element

---
+++ Arrays/SecondLargestNum.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[10],res[10], i, j, k=0, n, temp;
+
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+
+    printf("Enter the array elements:");
+    for( i = 0; i< n ; i++ )
+        scanf("%d",&arr[i]);
+
+    for( i=0; i<n; i++ ){
+        
+        for( j=0; j<n; j++ ){
+            if( arr[j] < arr[j+1] ){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("The Second Largest Element is:");
+    printf(" %d ",arr[1]);
+
+}
