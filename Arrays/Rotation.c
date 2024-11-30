d5e83add446f039c4e7a36ccb7aecd07a92677af
anveshshettyy <thomigang03@gmail.com>
2024-11-30 15:26:27 +0530

    Arrays

--- Arrays/Rotation.c
+++ Arrays/Rotation.c
@@ -11,18 +11,12 @@ int main()
     for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    printf("Enter the position:");
-    scanf("%d", &k);
-
-    for (j = 0; j < k; j++)
+    temp = arr[0];
+    for (i = 1; i < n; i++)
     {
-        temp = arr[0];
-        for (i = 1; i < n; i++)
-        {
-            arr[i - 1] = arr[i];
-        }
-        arr[n - 1] = temp;
+        arr[i - 1] = arr[i];
     }
+    arr[n - 1] = temp;
 
     printf("The rotated elemenst are:");
     for (i = 0; i < n; i++)
