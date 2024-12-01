599702c539c3f43234670ab6354022c441f7fbad
anveshshettyy <thomigang03@gmail.com>
2024-12-01 11:37:57 +0530

    bubble sort

---
+++ Arrays/BubbleSort.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+void readArray( int arr[] , int n ){
+    int i;
+    for( i=0; i<n ; i++){
+        scanf("%d",&arr[i]);
+    }
+}
+
+void printArray(int arr[], int n){
+    int i;
+    for( i=0; i<n ; i++){
+        printf(" %d \n",arr[i]);
+    }
+}
+
+void swap( int *p, int *q ){
+    int temp = *p;
+    *p = *q;
+    *q = temp;
+}
+
+void bubbleSort(int arr[], int n){
+    int i, j,status;
+
+    for( i=0; i<n; i++ ){
+        status = 0;
+        for( j=0; j<n-1-i; j++){
+            if( arr[j] > arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+                status = 1;
+            }
+                
+
+        }
+        if( status == 0 )
+            break;
+    }
+}
+
+int main() {
+    int arr[100];
+    int n;
+
+    printf("Enter Size:");
+    scanf("%d",&n);
+
+    printf("Enter the elements of array:");
+    readArray(arr, n);
+
+    printf("Initital arrray is:");
+    printArray(arr, n);
+
+    bubbleSort(arr , n);
+
+    printf("Sorted array is:");
+    printArray(arr, n);
+
+
+    return 0;
+}
---
+++ Arrays/BubbleSortCustomized.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+void readArray(int arr[], int n) {
+    for( int i=0; i<n ; i++){
+        scanf("%d",&arr[i]);
+    }
+}
+
+void printArray( int arr[], int n ){
+    for(int i=0; i<n; i++ )
+        printf(" %d ",arr[i]);
+}
+
+void swap( int *p, int *q ){
+    int temp;
+    temp = *p;
+    *p = *q;
+    *q= temp;
+}
+
+void Sorting( int arr[] , int n,int k){
+    int i, j, status;
+    for( i=0; i<k-1; i++ ){
+        status = 0;
+        for( j=0; j<k-i-1; j++ ){
+            if( arr[j]>arr[j+1]){
+                swap( &arr[j], &arr[j+1] );
+                status = 1;
+            }
+        }
+        if( status == 0 )
+            break;
+    }
+    for( k; k<n-1; k++ ){
+        status = 0;
+        for( j=k; j<n-i-1; j++ ){
+            if( arr[j]<arr[j+1]){
+                swap( &arr[j], &arr[j+1] );
+                status = 1;
+            }
+        }
+        if( status == 0 )
+            break;
+    }
+}
+
+
+int main() {
+    int n,k, arr[10];
+
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+
+    printf("Enter the elements of the array:");
+    readArray( arr,  n);
+
+
+    printf("Enter k:");
+    scanf("%d",&k);
+
+    printf("Before Sorting:");
+    printArray( arr , n);
+
+    Sorting( arr , n , k);
+
+    printf("\nAfter Sorting:");
+    printArray(arr , n);
+
+
+    return 0;
+}
