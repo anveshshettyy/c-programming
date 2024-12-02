db6238cbacf8d6649a9216ef7eab1870cc1831de
anveshshettyy <thomigang03@gmail.com>
2024-12-02 12:25:04 +0530

    sub array

--- Arrays/SubArrayK.c
+++ Arrays/SubArrayK.c
@@ -14,30 +14,29 @@ void printArray(int arr[], int n)
         printf(" %d ", arr[i]);
 }
 
-void SubArray(int arr[], int n, int k){
-    int i, j;
+// void SubArray(int arr[], int n, int k){
+//     int i, j;
     
 
 
-}
-
-
-// void SubArray(int arr[], int n, int k)
-// {
-//     int i, j, curr_sum, max_sum;
-//     curr_sum = 0;
-//     for( i=0; i<k; i++ ){
-//         curr_sum = curr_sum + arr[i]; 
-//     }
-//     max_sum = curr_sum;
-//     for( i=k ; i<n ; i++ ){
-//         curr_sum = curr_sum + arr[i] - arr[i-k];
-//         if( curr_sum > max_sum )
-//             max_sum = curr_sum;
-//     }
-
-//     printf("%d",max_sum);
 // }
+
+
+void SubArray(int arr[], int n, int k)
+{
+    int i, j, curr_sum, max_sum;
+    curr_sum = 0;
+    for( i=0; i<k; i++ ){
+        curr_sum = curr_sum + arr[i];
+    }
+    max_sum = curr_sum;
+    for( i=k ; i<n ; i++ ){
+        curr_sum = curr_sum + arr[i] - arr[i-k];
+        if( curr_sum > max_sum )
+            max_sum = curr_sum;
+    }
+    printf("%d",max_sum);
+}
 
 int main()
 {
