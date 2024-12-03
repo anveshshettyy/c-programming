b878fa17870752b139f5696bdb10cf2c8c551ece
anveshshettyy <thomigang03@gmail.com>
2024-12-03 17:38:42 +0530

    changes

--- Arrays/BuyAndSell.c
+++ Arrays/BuyAndSell.c
@@ -1,3 +1,18 @@
 #include<stdio.h>
 
-int main(  )
+int main(){
+
+    int n, stocks[10], i;
+    printf("Enter the size of stocks:");
+    scanf("%d",&n);
+
+    printf("Enter the stocks: ");
+    for( i=0; i<n, i++ ){
+        scanf("%d",&stocks[i]);
+    }
+
+    
+
+
+    return 0;
+}
