732228d8b781501af1acfa32d307b3e94262979b
anveshshettyy <thomigang03@gmail.com>
2024-12-05 19:27:44 +0530

    change

--- Strings/LastStringLength.c
+++ Strings/LastStringLength.c
@@ -1,23 +1,20 @@
 #include<stdio.h>
 
+int lengthOfLastWord(char* s) {
+    
+}
+
 int main() {
 
     char str[100], *s;
-    int length=0,i;
+    int length=0,i,count;
 
     printf("Enter the string: ");
     scanf("%s",str);
 
     s = str;
-
-    // for( i=0; *s[i]; i++ )
-    //     length++;
-
-    printf("%s", s);
-
-
-
-
+   
+    lengthOfLastWord( s );
 
     return 0;
 }
