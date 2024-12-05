ec8c532636a7cef4ca4f3a00eb50d8db33a08004
anveshshettyy <thomigang03@gmail.com>
2024-12-05 21:28:21 +0530

    changes

--- Strings/EqualOccurence.c
+++ Strings/EqualOccurence.c
@@ -1,37 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 #define SIZE 26
-int main(){
+int main()
+{
 
     char str[100];
-    int count[SIZE] = { 0 };
-    int i, flag = 1, frequency=0;
-    
-    printf("Enter a string: ");
-    scanf("%[^\n]s",str);
+    int count[SIZE] = {0};
+    int i, flag = 1, frequency = 0;
 
-    for( i=0; str[i] ; i++ )
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+
+    for (i = 0; str[i]; i++)
     {
-        if( str[i] >= 65 && str[i] <= 90 )
+        if (str[i] >= 65 && str[i] <= 90)
             count[str[i] - 'A']++;
 
-        else if( str[i] >= 97 && str[i] <= 122 )
+        else if (str[i] >= 97 && str[i] <= 122)
             count[str[i] - 'a']++;
     }
 
-    for( i=0; i<SIZE; i++ )
+    for (i = 0; i < SIZE; i++)
     {
-        if( count[i] > 0 ){
-            if( frequency == 0 )
+        if (count[i] > 0)
+        {
+            if (frequency == 0)
                 frequency = count[i];
-            else if( count[i] != frequency  )
+            else if (count[i] != frequency)
             {
                 flag = 0;
                 break;
-            } 
+            }
         }
     }
 
-    if( flag )
+    if (flag)
         printf("Equal Occurence");
     else
         printf("Unequal Occurence");
