6276fc7ff33842cbf167646e5dc6db9be2e762c2
anveshshettyy <thomigang03@gmail.com>
2024-12-05 20:08:02 +0530

    last string

--- Strings/LastStringLength.c
+++ Strings/LastStringLength.c
@@ -1,26 +1,33 @@
 #include<stdio.h>
 
 int lengthOfLastWord(char* s) {
-    int length=0, i;
-    for( i=0; s[i] ; s++ )
-        length++;
+    int length=0, i, count = 0;
+    for( i=0; s[i] ; i++ )  
+            length++;
 
-    printf("%d",length);
+    for( i=length - 1; i>=0 && s[i] == ' '; i--);
+
+    for( i ; i>=0 && s[i] != ' ' ; i-- )
+        count++;
+
+    return count;
 }
 
 int main() {
 
     char str[100], *s;
-    int length=0,i,count;
+    int count;
 
     printf("Enter the string: ");
-    scanf("%s",str);
+    scanf("%[^\n]s",str);
 
     while( getchar() != '\n' );
 
     s = str;
    
-    lengthOfLastWord( s );
+    count = lengthOfLastWord( s );
+
+    printf("Length of the last word: %d", count);
 
     return 0;
 }
