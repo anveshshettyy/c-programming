b0260e98b403fd95ccdfa1fdf75ac9a93e4e0a53
anveshshettyy <thomigang03@gmail.com>
2024-12-05 21:11:53 +0530

    Equal occurence

---
+++ Strings/EqualOccurence.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+
+    char str[100];
+    int count[100] = { 0 };
+    int i, flag = 1, frequency=0;
+    
+    printf("Enter a string: ");
+    scanf("%[^\n]s",str);
+
+    for( i=0; str[i] ; i++ )
+    {
+        if( str[i] >= 65 && str[i] <= 90 )
+            count[str[i] - 'A']++;
+
+        else if( str[i] >= 97 && str[i] <= 122 )
+            count[str[i] - 'a']++;
+    }
+
+    for( i=0; i<26; i++ )
+    {
+        if( count[i] > 0 ){
+            if( frequency == 0 )
+                frequency = count[i];
+            else if( count[i] != frequency  )
+            {
+                flag = 0;
+                break;
+            }
+                
+        }
+    }
+    if( flag )
+        printf("Equal Occurence");
+    else
+        printf("Unequal Occurence");
+
+    return 0;
+}
