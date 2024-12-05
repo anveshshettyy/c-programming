746425a39a423d63cd57c89d855e8fdd63104103
anveshshettyy <thomigang03@gmail.com>
2024-12-05 20:41:30 +0530

    frequency

--- Strings/FrequencyOfStrings.c
+++ Strings/FrequencyOfStrings.c
@@ -2,13 +2,29 @@
 
 int main() {
     char str[100];
+    int count[26] = {0};
+    int i;
 
     printf("Enter a string: ");
     scanf("%[^\n]s",str);
 
-    while( getchar() != '\n' );
+    for( i=0; str[i]; i++ )
+    {
+        if( str[i] >= 65 && str[i] <= 90 )
+            count[str[i] - 'A']++;
+        
+        else if( str[i] >= 97 && str[i] <= 122 )
+            count[str[i] - 'a']++;
+        
+    }
 
-    
+    for( i=0; i<26; i++ )
+    {
+        if( count[i] > 0 )
+            printf(" %c ( %d )... ", i + 'a', count[i]);
+    }
+
+
 
 
     return 0;
