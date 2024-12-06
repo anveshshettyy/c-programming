23f4ab31562203b1cec693e5dc04cc94633e8b70
anveshshettyy <thomigang03@gmail.com>
2024-12-06 10:03:09 +0530

    string

--- Strings/SubStringChar.c
+++ Strings/SubStringChar.c
@@ -3,14 +3,13 @@
 int main ()
 {
 
-    char str[100];
+    char str[100], sub[100];
 
     printf("Enter a string: ");
     scanf("%[^/n]s",str);
 
     printf("Enter a sub string: ");
-    scanf("")
-
+    scanf("%[^/n]s",sub);
 
     return 0;
 }
