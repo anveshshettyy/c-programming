2bde16a1dfb3c53c00ce7c34860ddea1f81ce39b
anveshshettyy <thomigang03@gmail.com>
2024-12-06 16:06:37 +0530

    changes

--- Recursion/ReversingLogic2.c
+++ Recursion/ReversingLogic2.c
@@ -1,26 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 
-char *RevString( char *p )
+char rev_word(char *q){
+    int z;
+    if(*q==' ')
+        z=0;
+
+    else if(*q=='\0')
+        z=0;
+
+    else
+        z=rev_word(q+1) + 1;
+    if(z)
+        putchar(*q);
+    return z;
+    
+}
+char *rev(char *t)
 {
     static char a[100];
     static int i;
 
-    if( *p )
+    if(*t)
     {
-        RevString(p+1);
-        a[i++] = *p;
+        rev(t+1);
+        a[i++]=*t;
     }
     return a;
 }
 
 int main()
 {
-    char str[100];
-
-    printf("Enter a string: ");
-    scanf("%[^\n]s", str);
-
-    printf("Reversed String is %s",RevString(str));
+    char *t=(char *)malloc(100*sizeof(char *));
+    char *p=NULL;
+    int a;
+    printf("Enter String: ");
+    scanf("%[^\n]s",t);
+    p=rev(t);
+   
+    while(*p){
+        a=rev_word(p);
+        p=p+a;
+        if(*p==' '){
+            putchar(' ');
+            p++;
+        }
+       
+        }
 
     return 0;
 }
