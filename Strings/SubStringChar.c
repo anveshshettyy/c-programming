12180ff604dfb5082deb79a1a94c5b529842912f
anveshshettyy <thomigang03@gmail.com>
2024-12-06 09:59:36 +0530

    a

---
+++ Strings/SubStringChar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main ()
+{
+
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%[^/n]s",str);
+
+    printf("Enter a sub string: ");
+    scanf("")
+
+
+    return 0;
+}
