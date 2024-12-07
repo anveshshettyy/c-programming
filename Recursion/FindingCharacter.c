f38b808a0cbf26140e849ce7d9872bf8b13b293b
anveshshettyy <thomigang03@gmail.com>
2024-12-07 20:34:54 +0530

    changes

---
+++ Recursion/FindingCharacter.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+// char FindChar(char  str[] )
+// {
+//     if( str == ' ' )
+//         return 0;
+
+//     else FindChar(str++);
+// }
+
+int main()
+{
+
+    char str[100];
+    char chr;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]s",str);
+
+    printf("Enter a character ");
+    scanf("%c",chr);
+
+    printf("%c",char);
+
+    // FindChar( str );
+
+
+
+    return 0;
+}
+
