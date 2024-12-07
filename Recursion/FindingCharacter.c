a5a09cbf1d5cf962ef405c898811c61ebd544e6e
anveshshettyy <thomigang03@gmail.com>
2024-12-07 20:51:19 +0530

    finding sub character

--- Recursion/FindingCharacter.c
+++ Recursion/FindingCharacter.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
 
-int FindChar(char  str[], char chr )
+int FindChar(char  str[], char chr, int i )
 {
-    int i = 0;
 
-    if(!str)
+    if(str[i] == '\0')
         return 0;
 
-    else
-    {
-        if( str[i] == chr )
-        {
-            return i;
-        }
-        FindChar(str[i+1]);
-    }
+    if( str[i] == chr )
+        return i;
+
+    return FindChar(str, chr, i+1);
 }
 
 int main()
@@ -22,7 +17,7 @@ int main()
 
     char str[100];
     char chr;
-    int i;
+    int i=0;
 
     printf("Enter a string: ");
     scanf("%[^\n]s",str);
@@ -38,10 +33,9 @@ int main()
     //         printf("Character found at index %d \n",i);
     // }
 
-    int index = FindChar( str, chr );
+    int index = FindChar( str, chr, i );
 
     printf("Character found at index %d \n",index);
-
 
     return 0;
 }
